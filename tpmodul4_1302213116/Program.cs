767519a5faf1d57f7d32a0972d195eb84f95cfc3
Raychan69 <fasyaraihan82@gmail.com>
2023-03-13 01:21:18 +0700

    menambahkan implementasi dengan table-driven

--- tpmodul4_1302213116/Program.cs
+++ tpmodul4_1302213116/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+class KodePos
+{
+    private String[] Kelurahan = { "Batununggal", "Kujangsari", "Mengger", "Wates", "Cijaura", "Jatisari", "Margasari", "Sekejati", "Kebonwaru", "Maleer", "Samoja" };
+    private String[] Kode_Pos = { "40266", "40287", "40267", "40256", "40287", "40286", "40286", "40286", "40272", "40274", "40273" };
+
+    public string GetKodePos(String Kelurahan)
+    {
+        foreach (String data_kelurahan in this.Kelurahan)
+        {
+            if(Kelurahan == data_kelurahan)
+            {
+                return "Kode Pos : "+Kode_Pos[Array.IndexOf(this.Kelurahan, data_kelurahan)];
+            }
+        }
+        return "Data Tidak Ditemukan";
+    }
+
+}
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        KodePos kode = new KodePos();
+
+        Console.Write("Masukan Nama Kelurahan : "); string Kelurahan = Console.ReadLine();
+
+        Console.WriteLine(kode.GetKodePos(Kelurahan));
+    }  
+}
