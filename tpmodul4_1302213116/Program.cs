897327478beb95e68ff3907bd18f1b29cf64584d
Raychan69 <fasyaraihan82@gmail.com>
2023-03-13 02:20:31 +0700

    menambahkan implementasi dengan state-based construction

--- tpmodul4_1302213116/Program.cs
+++ tpmodul4_1302213116/Program.cs
@@ -1,6 +1,4 @@
-﻿
-
-class KodePos
+﻿class KodePos
 {
     private String[] Kelurahan = { "Batununggal", "Kujangsari", "Mengger", "Wates", "Cijaura", "Jatisari", "Margasari", "Sekejati", "Kebonwaru", "Maleer", "Samoja" };
     private String[] Kode_Pos = { "40266", "40287", "40267", "40256", "40287", "40286", "40286", "40286", "40272", "40274", "40273" };
@@ -18,6 +16,35 @@ class KodePos
     }
 
 }
+
+class DoorMachine
+{
+    public enum state { TERKUNCI, TERBUKA };
+    private state pintu = state.TERKUNCI;
+    String[] status = { "Pintu Terkunci", "Pintu Tidak Terkunci" }; 
+
+    public void set_status_pintu(String aksi)
+    {
+        switch (pintu)
+        {
+            case state.TERKUNCI:
+                if(aksi == "BukaPintu")
+                {
+                    pintu = state.TERBUKA;      
+                }
+                Console.WriteLine(status[(int)pintu]);
+            break;
+            case state.TERBUKA:
+                if (aksi == "KunciPintu")
+                {
+                    pintu = state.TERKUNCI;
+                }
+                Console.WriteLine(status[(int)pintu]);
+            break;
+        }
+    }
+}
+
 public class Program
 {
     public static void Main(String[] args)
@@ -27,5 +54,13 @@ public class Program
         Console.Write("Masukan Nama Kelurahan : "); string Kelurahan = Console.ReadLine();
 
         Console.WriteLine(kode.GetKodePos(Kelurahan));
+
+        DoorMachine obj = new DoorMachine();
+
+        obj.set_status_pintu("BukaPintu");
+        obj.set_status_pintu("KunciPintu");
+        obj.set_status_pintu("KunciPintu");
+        obj.set_status_pintu("BukaPintu");
+
     }  
 }
